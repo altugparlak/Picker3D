b09b3df0b7c9511dab181ba162c9a1194790ebef
Altug Parlak <alparlak@hotmail.com>
2022-09-08 22:39:11 +0300

    StartScene01

--- Assets/Scripts/PickerMovement.cs
+++ Assets/Scripts/PickerMovement.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PickerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 1f;
-
+    [SerializeField] private GameObject startImages;
     public Joystick joystick;
     public CharacterController characterController;
+
+    private bool firstTouch = false;
 
 
     private void Start()
@@ -18,6 +20,20 @@ public class PickerMovement : MonoBehaviour
 
     void Update()
     {
+
+        if (!firstTouch)
+        {
+            if (Input.touchCount > 0)
+            {
+                startImages.SetActive(false);
+                firstTouch = true;
+            }
+        }
+        else
+        {
+            MoveForward();
+        }
+
 
         MoveWithController();
 
@@ -33,4 +49,11 @@ public class PickerMovement : MonoBehaviour
             characterController.Move(-direction * movementSpeed * Time.deltaTime);
         }
     }
+
+    private void MoveForward()
+    {
+        Vector3 direction = new Vector3(0f, 0f, -1f);
+        characterController.Move(direction * movementSpeed * Time.deltaTime);
+    }
+
 }
