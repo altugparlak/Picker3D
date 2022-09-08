32dfede7b1d3a7d423ed11a4a0dcac191f65b1c3
Altug Parlak <alparlak@hotmail.com>
2022-09-08 21:04:10 +0300

    MovementUpdate

--- Assets/Scripts/PickerMovement.cs
+++ Assets/Scripts/PickerMovement.cs
@@ -6,38 +6,31 @@ public class PickerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 1f;
 
-    private Touch touch;
-    Rigidbody rb;
+    public Joystick joystick;
+    public CharacterController characterController;
+
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        characterController.enabled = true;
+
     }
 
     void Update()
     {
 
-        if (Input.touchCount > 0)
+        MoveWithController();
+
+    }
+
+    private void MoveWithController()
+    {
+        float moveHorizontal = joystick.Horizontal;
+        Vector3 direction = new Vector3(moveHorizontal, 0f, 0f);
+
+        if (direction.magnitude >= 0.1f)
         {
-            touch = Input.GetTouch(0);
-            Debug.Log(touch.position);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-
-            if (touch.phase == TouchPhase.Moved)
-            {
-                //transform.position = new Vector3(
-                //    transform.position.x + touch.deltaPosition.x * -movementSpeed,
-                //    transform.position.y,
-                //    transform.position.z);
-                //rb.velocity = new Vector2(direction.x, direction.y) * movementSpeed;
-
-
-                if (touch.phase == TouchPhase.Ended)
-                {
-                    rb.velocity = Vector2.zero;
-                }
-            }
+            characterController.Move(-direction * movementSpeed * Time.deltaTime);
         }
-
     }
 }
