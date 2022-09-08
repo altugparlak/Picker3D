75d9aeff7b9b67e97607ed46d3503064d71cc01c
Altug Parlak <alparlak@hotmail.com>
2022-09-08 20:37:41 +0300

    new

--- Assets/Scripts/PickerMovement.cs
+++ Assets/Scripts/PickerMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PickerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float movementSpeed = 1f;
+
+    private Touch touch;
+    Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            Debug.Log(touch.position);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                //transform.position = new Vector3(
+                //    transform.position.x + touch.deltaPosition.x * -movementSpeed,
+                //    transform.position.y,
+                //    transform.position.z);
+                //rb.velocity = new Vector2(direction.x, direction.y) * movementSpeed;
+
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    rb.velocity = Vector2.zero;
+                }
+            }
+        }
+
     }
 }
