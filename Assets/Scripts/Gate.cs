cebe50cc753074b691a2997383d888be458753b6
Altug Parlak <alparlak@hotmail.com>
2022-09-10 22:31:50 +0300

    updt

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] private GameObject gateText;
+    PickerMovement pickerMovement;
+
+    void Start()
+    {
+        pickerMovement = FindObjectOfType<PickerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PickerCanMove()
+    {
+        pickerMovement.canMoveForward = true;
+        gateText.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/LevelUIHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUIHandler : MonoBehaviour
+{
+    [SerializeField] private Image levelBox1;
+    [SerializeField] private Image levelBox2;
+    [SerializeField] private Image levelBox3;
+
+    void Start()
+    {
+        //levelBox1.color = new Color32(255, 120, 66, 255);
+        levelBox1.color = new Color32(255, 255, 255, 255);
+        levelBox2.color = new Color32(255, 255, 255, 255);
+        levelBox3.color = new Color32(255, 255, 255, 255);
+
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
